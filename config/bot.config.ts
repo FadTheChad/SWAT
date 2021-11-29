432a1e6dac7a202f683444d04bf4efc38f9418cc
Codeize <mrcodeize@gmail.com>
2021-11-29 12:00:35 +0000

    Launched SWAT.

---
+++ config/bot.config.ts
@@ -0,0 +1,43 @@
+import { Intents, PermissionString } from "discord.js";
+
+export default {
+	version: "1.0.0",
+	admins: ["668423998777982997"],
+
+	supportServer: "https://discord.gg/7syTGCkZs8",
+	minimalInvite:
+		"https://discord.com/api/oauth2/authorize?client_id=914290270508572692&permissions=292556957910&redirect_uri=https%3A%2F%2Fdiscord.gg%7syTGCkZs8&scope=applications.commands%20bot",
+	recommendedInvite:
+		"https://discord.com/api/oauth2/authorize?client_id=914290270508572692&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%7syTGCkZs8&scope=applications.commands%20bot",
+
+	hastebin: "https://h.inv.wtf",
+
+	colors: {
+		primary: "5865F2",
+		success: "57F287",
+		warning: "FEE75C",
+		error: "ED4245"
+	},
+
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_BANS,
+		Intents.FLAGS.GUILD_INVITES,
+		Intents.FLAGS.GUILD_MEMBERS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+	],
+
+	requiredPermissions: [
+		"EMBED_LINKS",
+		"SEND_MESSAGES",
+		"USE_EXTERNAL_EMOJIS"
+	] as PermissionString[],
+
+	apiKeys: {},
+
+	emojis: {
+		checkMark: ":white_check_mark:",
+		xMark: ":x:"
+	}
+};
---
+++ readme.md
@@ -0,0 +1,14 @@
+# SWAT
+
+This is SWAT, a moderation and automoderation bot for Discord. Made for all types of server setups and made to give moderators plenty of choice when handling different moderation scenarios.
+
+Version is currently **__0 Dev__**. This means that the core functionality of the bot is still largely untested and under developed, there's a high probability that the bot will break.
+To keep updated with the bot and it's development, join the [Support Server](#support).
+
+## Credits
+
+[SwitchbladeBot](https://github.com/SwitchbladeBot) for allowing the use of their widget service.
+[xPolar](https://github.com/xPolar) for providing the base of the bot.
+
+## Support
+[![widget](https://invidget.switchblade.xyz/854739172580655134)](https://discord.gg/7syTGCkZs8)
