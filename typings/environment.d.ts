d99bf44e61428a363cf01a42b251c3e3abf11669
Codeize <mrcodeize@gmail.com>
2021-12-04 00:05:21 +0000

    Update environment.d.ts

--- typings/environment.d.ts
+++ typings/environment.d.ts
@@ -4,7 +4,7 @@ declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
 			MONGO_URI: string;
-			NODE_ENV: "development" | "production";
+			NODE_ENV: "development" | "production" | "lockdown";
 			DISCORD_TOKEN: string;
 			SENTRY_DSN: string;
 		}
